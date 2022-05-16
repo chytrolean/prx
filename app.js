755b843abf63fd223f0c7c23e8bdb0a65e2920a5
chytrolean <84605452+chytrolean@users.noreply.github.com>
2022-05-16 08:05:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,13 +7,65 @@ function play(){
 const app = Vue.createApp({
     data(){
         return{
-            song: "cock",
-            stop:true
+            current: {},
+            index: 0,
+            stop:true,
+            list:true,
+            isPlaying: false,
+            search: "",
+            songs: [
+                {
+                    name: "Blessed",
+                    author:"Viktor Sheen",
+                    src: 'Blessed.mp3'
+                },
+                {
+                    name: "Piko v nose",
+                    author:"Steve Sniff",
+                    src: 'piko-v-nose.mp3'
+                },
+                {
+                    name: "Hannah Montana VIP",
+                    author:"Calin",
+                    src: 'Hannah-Montana-VIP.mp3'
+                },
+            ],
+            musicPlayer: new Audio()
         }
     },
     methods:{
-        play(){
-            this.stop = !this.stop
+        play(song){
+            if(typeof song.src != 'undefined'){
+                this.current = song
+                this.musicPlayer.src = this.current.src
+            }
+            console.log(typeof(this.current.src))
+
+            this.stop = false
+            this.musicPlayer.play()
+            this.isPlaying = true
+        },
+        stoped(){
+            this.musicPlayer.pause()
+            this.stop = true
+        },
+        listed(){
+            this.list = !this.list
+        },
+        seek(){
+            this.list = !this.list
+            this.$nextTick(() => this.$refs.inp.focus())  
+      }
+    },
+    created(){
+        this.current = this.songs[this.index]
+        this.musicPlayer.src = this.current.src
+    },
+    computed: {
+        filtered(){
+            return this.songs.filter(p => {
+                return p.name.toLowerCase().indexOf(this.search.toLowerCase()) != - 1;
+            })
         }
     }
 })
